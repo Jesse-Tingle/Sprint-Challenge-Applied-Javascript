35c6ca585a725750bc6f1562ba60d1ad619fb0d4
Jesse-Tingle <jtingle0@gmail.com>
2019-08-18 21:39:24 -0400

    completed cards index.js file

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,29 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+
+// let ax = axios.get('https://lambda-times-backend.herokuapp.com/topics');
+// let git = axios.get('https://api.github.com/users/Jesse-Tingle')
+// console.log(ax);
+// console.log(git);
+
+// let tabSection = document.querySelector('.tabs');
+// let tabArray = [];
+// axios.get('https://lambda-times-backend.herokuapp.com/topics')
+//     .then(topics => {
+//         tabArray = topics.data.map(topic => topic)
+//     .catch(error => {
+//         console.error(error);
+//     });
+//     })
+
+// function TabCreator() {
+//     let tabDiv = document.createElement('div');
+//     tabDiv.classList.add('tab');
+
+//     return tabDiv;
+// }
+
+// console.log(TabCreator());
